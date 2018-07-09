9d6dede81c4a264adecb7a6816657cea843343d9
Ajith05Thala <40866122+Ajith05Thala@users.noreply.github.com>
2018-07-09 14:54:47 +0530

    Update vow.java

--- vow.java
+++ vow.java
@@ -1,20 +1,28 @@
 import java.util.*;
-import java.io.*;
-class vow
+class odd
 {
     public static void main(String args[])
     {
+        
         Scanner sc=new Scanner(System.in);
         char s=sc.next().charAt(0);
-        if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+         if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
         {
             System.out.print("vowel");
         }
         else
+        if((s>='a'&&s<='z')||(s>='A'&&s<='z'))
         {
             System.out.print("consonent");
         }
-    }
+        else
+        {
+            System.out.print("Invalid");
+        }
+        }
 }
+
+
+
 
 
