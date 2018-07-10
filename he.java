6214c3a89ab71128007df68540553537ad7e0818
Ajith05Thala <40866122+Ajith05Thala@users.noreply.github.com>
2018-07-10 23:05:16 +0530

    Create he.java

---
+++ he.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class he
+{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int i;
+        for(i=0;i<s;i++)
+        {
+            System.out.println("Hello");
+        }
+        
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
