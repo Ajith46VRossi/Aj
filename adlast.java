13b1c0e49203dda6cc1d39a7a2d8cacf70fce420
Ajith05Thala <40866122+Ajith05Thala@users.noreply.github.com>
2018-07-15 21:18:35 +0530

    Create adlast.java

---
+++ adlast.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class adlast
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        StringBuffer sb= new StringBuffer(s);
+        sb.append(".");
+        System.out.println(sb); 
+        
+        
+    }
+}
