a2708fb28583132bfba8c8a46e8ccc0de50bbedb
Ajith05Thala <40866122+Ajith05Thala@users.noreply.github.com>
2018-07-15 18:56:54 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class rev
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String reverse="";
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            reverse=reverse+s.charAt(i);
+        }
+        System.out.print(reverse);
+    }
+}
