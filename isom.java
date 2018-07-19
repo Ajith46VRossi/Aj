7a5657848b96ed599c323b977beabacb0136a6da
Ajith05Thala <40866122+Ajith05Thala@users.noreply.github.com>
2018-07-19 22:28:12 +0530

    Create isom.java

---
+++ isom.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class isom
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String s1=sc.next();
+        HashSet<String> hs=new HashSet<String>();
+        for(int i=0;i<s.length();i++)
+        {
+            hs.add(Character.toString(s.charAt(i)));
+        }
+        HashSet<String> hs1=new HashSet<String>();
+        for(int i=0;i<s1.length();i++)
+        {
+            hs1.add(Character.toString(s1.charAt(i)));
+        }
+        if(hs.size()==hs1.size())
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("No");
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
